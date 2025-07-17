66bde6b54fe9b8069e2fcd9822c3d4d2c7cadde6
Magesh Kuppan <tkmagesh77@gmail.com>
2025-07-17 12:32:33 +0530

    05-exercise solution

--- 02-concurrency/00-exercises/05-exercise-concurrent.go
+++ 02-concurrency/00-exercises/05-exercise-concurrent.go
@@ -9,34 +9,31 @@ import (
 	"sync"
 )
 
-// communicate by sharing memory
-var primes []int
-var mutex sync.Mutex
-
 func main() {
-	var wg sync.WaitGroup
-	for no := 2; no <= 100; no++ {
-		wg.Add(1)
-		go func() {
-			defer wg.Done()
-			checkIfPrime(no)
-		}()
-	}
-	wg.Wait()
-	for _, primeNo := range primes {
+	ch := make(chan int)
+	go func() {
+		var wg sync.WaitGroup
+		for no := 2; no <= 100; no++ {
+			wg.Add(1)
+			go func() {
+				defer wg.Done()
+				checkIfPrime(no, ch)
+			}()
+		}
+		wg.Wait()
+		close(ch)
+	}()
+	for primeNo := range ch {
 		fmt.Println("Prime No :", primeNo)
 	}
+
 }
 
-func checkIfPrime(no int) {
+func checkIfPrime(no int, ch chan int) {
 	for i := 2; i <= (no / 2); i++ {
 		if no%i == 0 {
 			return
 		}
 	}
-	mutex.Lock()
-	{
-		primes = append(primes, no)
-	}
-	mutex.Unlock()
+	ch <- no
 }
