e50ef61c97a6b0c9d2b12ee642c7e5ece9f3ab61
Magesh Kuppan <tkmagesh77@gmail.com>
2025-07-17 10:11:50 +0530

    04-exercise solution

--- 02-concurrency/00-exercises/04-exercise-concurrent.go
+++ 02-concurrency/00-exercises/04-exercise-concurrent.go
@@ -9,24 +9,33 @@ import (
 	"sync"
 )
 
+var primes []int
+var mutex sync.Mutex
+
 func main() {
 	var wg sync.WaitGroup
 	for no := 2; no <= 100; no++ {
 		wg.Add(1)
 		go func() {
 			defer wg.Done()
-			printIfPrime(no)
+			checkIfPrime(no)
 		}()
 	}
 	wg.Wait()
+	for _, primeNo := range primes {
+		fmt.Println("Prime No :", primeNo)
+	}
 }
 
-func printIfPrime(no int) {
+func checkIfPrime(no int) {
 	for i := 2; i <= (no / 2); i++ {
 		if no%i == 0 {
 			return
 		}
 	}
-	fmt.Println("Prime No :", no)
-	return
+	mutex.Lock()
+	{
+		primes = append(primes, no)
+	}
+	mutex.Unlock()
 }
