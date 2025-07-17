fcd53260e74889a9aded9a4b1c8f07eb487bc441
Magesh Kuppan <tkmagesh77@gmail.com>
2025-07-17 14:42:22 +0530

    buffered channels

---
+++ 02-concurrency/10-buffered-channels/22-demo.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	ch := make(chan int, 3)
+	fmt.Printf("cap(ch) : %d, len(ch) : %d\n", cap(ch), len(ch))
+	ch <- 10
+	fmt.Printf("cap(ch) : %d, len(ch) : %d\n", cap(ch), len(ch))
+	ch <- 20
+	fmt.Printf("cap(ch) : %d, len(ch) : %d\n", cap(ch), len(ch))
+	ch <- 30
+	fmt.Printf("cap(ch) : %d, len(ch) : %d\n", cap(ch), len(ch))
+
+	fmt.Println("data from ch :", <-ch)
+	fmt.Printf("cap(ch) : %d, len(ch) : %d\n", cap(ch), len(ch))
+	fmt.Println("data from ch :", <-ch)
+	fmt.Printf("cap(ch) : %d, len(ch) : %d\n", cap(ch), len(ch))
+	fmt.Println("data from ch :", <-ch)
+	fmt.Printf("cap(ch) : %d, len(ch) : %d\n", cap(ch), len(ch))
+
+}
